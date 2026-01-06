48a72fa14816fe3681875e3d88d4ab78587059ea
Jake Soenneker <jake@soenneker.com>
2026-01-06 17:10:03 -0600

    Initial commit

---
+++ test/Soenneker.Enums.InitializationModes.Tests/InitializationModeTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Enums.InitializationModes.Tests;
+
+public sealed class InitializationModeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
