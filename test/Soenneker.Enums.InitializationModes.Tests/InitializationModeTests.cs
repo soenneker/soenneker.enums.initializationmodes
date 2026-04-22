893b97324c241356425b3ed34d2bf28808c16895
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:45 -0500

    TUnit conversion

--- test/Soenneker.Enums.InitializationModes.Tests/InitializationModeTests.cs
+++ test/Soenneker.Enums.InitializationModes.Tests/InitializationModeTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Enums.InitializationModes.Tests;
 
 public sealed class InitializationModeTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
